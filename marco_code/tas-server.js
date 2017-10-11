3e5f5427f29baeadca04e53cba04c05b130520c5
mdduran <mdduran@outlook.com>
2017-10-11 16:34:02 -0400

    Server handles post with updated format

--- marco_code/tas-server.js
+++ marco_code/tas-server.js
@@ -25,9 +25,10 @@ app.post('/posttest', function(req, res){
 
 //Handle post requests to take in game input
 // 0 is red, 1 is yellow
-//post request convention is as follow: "/colorRowCol"
+//post request convention is as follow: "/colorSquare#"
+//square numbers are 0 - 8
 //post request will turn on the light at the given place on the board
-/** Board State: red11
+/** Board State: red0
  * 
  *        0 |   |
  *       ___|___|___
@@ -37,10 +38,10 @@ app.post('/posttest', function(req, res){
  *          |   |
  * 
  */
-app.post('/red11', function(req, res){
-  res.send("Color: Red, Row: 1, Col: 1");
+app.post('/red0', function(req, res){
+  res.send("Color: Red, Square: 0");
 })
- /** Board State: red12
+ /** Board State: red1
  * 
  *          | 0 |
  *       ___|___|___
@@ -50,10 +51,10 @@ app.post('/red11', function(req, res){
  *          |   |
  * 
  */
-app.post('/red12', function(req, res){
-  res.send("Color: Red, Row: 1, Col: 2");
+app.post('/red1', function(req, res){
+  res.send("Color: Red, Square: 0");
 })
- /** Board State: red13
+ /** Board State: red2
  * 
  *          |   | 0
  *       ___|___|___
@@ -63,10 +64,10 @@ app.post('/red12', function(req, res){
  *          |   |
  * 
  */
-app.post('/red13', function(req, res){
-  res.send("Color: Red, Row: 1, Col: 3");
+app.post('/red2', function(req, res){
+  res.send("Color: Red, Square: 2");
 })
- /** Board State: red21
+ /** Board State: red3
  * 
  *          |   |
  *       ___|___|___
@@ -76,10 +77,10 @@ app.post('/red13', function(req, res){
  *          |   |
  * 
  */
-app.post('/red21', function(req, res){
-  res.send("Color: Red, Row: 2, Col: 1");
+app.post('/red3', function(req, res){
+  res.send("Color: Red, Square: 3");
 })
- /** Board State: red22
+ /** Board State: red4
  * 
  *          |   |
  *       ___|___|___
@@ -89,10 +90,10 @@ app.post('/red21', function(req, res){
  *          |   |
  * 
  */
-app.post('/red22', function(req, res){
-  res.send("Color: Red, Row: 2, Col: 2");
+app.post('/red4', function(req, res){
+  res.send("Color: Red, Square: 4");
 })
- /** Board State: red23
+ /** Board State: red5
  * 
  *          |   |
  *       ___|___|___
@@ -102,10 +103,10 @@ app.post('/red22', function(req, res){
  *          |   |
  * 
  */
-app.post('/red23', function(req, res){
-  res.send("Color: Red, Row: 2, Col: 3");
+app.post('/red5', function(req, res){
+  res.send("Color: Red, Square: 5");
 })
- /** Board State: red31
+ /** Board State: red6
  * 
  *          |   |
  *       ___|___|___
@@ -115,10 +116,10 @@ app.post('/red23', function(req, res){
  *          |   |
  * 
  */
-app.post('/red31', function(req, res){
-  res.send("Color: Red, Row: 3, Col: 1");
+app.post('/red6', function(req, res){
+  res.send("Color: Red, Square: 6");
 })
- /** Board State: red32
+ /** Board State: red7
  * 
  *          |   |
  *       ___|___|___
@@ -128,10 +129,10 @@ app.post('/red31', function(req, res){
  *          |   |
  * 
  */
-app.post('/red32', function(req, res){
-  res.send("Color: Red, Row: 3, Col: 2");
+app.post('/red7', function(req, res){
+  res.send("Color: Red, Square: 7");
 })
- /** Board State: red33
+ /** Board State: red8
  * 
  *          |   |
  *       ___|___|___
@@ -141,10 +142,10 @@ app.post('/red32', function(req, res){
  *          |   |
  * 
  */
-app.post('/red33', function(req, res){
-  res.send("Color: Red, Row: 3, Col: 3");
+app.post('/red8', function(req, res){
+  res.send("Color: Red, Square: 8");
 })
- /** Board State: yellow11
+ /** Board State: yellow0
  * 
  *        1 |   |
  *       ___|___|___
@@ -154,10 +155,10 @@ app.post('/red33', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow11', function(req, res){
-  res.send("Color: yellow, Row: 1, Col: 1");
+app.post('/yellow0', function(req, res){
+  res.send("Color: yellow, Square: 0");
 })
- /** Board State: yellow12
+ /** Board State: yellow1
  * 
  *          | 1 |
  *       ___|___|___
@@ -167,10 +168,10 @@ app.post('/yellow11', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow12', function(req, res){
-  res.send("Color: Yellow, Row: 1, Col: 2");
+app.post('/yellow1', function(req, res){
+  res.send("Color: Yellow, Square: 1");
 })
- /** Board State: yellow13
+ /** Board State: yellow2
  * 
  *          |   | 1
  *       ___|___|___
@@ -180,10 +181,10 @@ app.post('/yellow12', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow13', function(req, res){
-  res.send("Color: Yellow, Row: 1, Col: 3");
+app.post('/yellow2', function(req, res){
+  res.send("Color: Yellow, Square: 2");
 })
- /** Board State: yellow21
+ /** Board State: yellow3
  * 
  *          |   |
  *       ___|___|___
@@ -193,10 +194,10 @@ app.post('/yellow13', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow21', function(req, res){
-  res.send("Color: Yellow, Row: 2, Col: 1");
+app.post('/yellow3', function(req, res){
+  res.send("Color: Yellow, Square: 3");
 })
- /** Board State: yellow22
+ /** Board State: yellow4
  * 
  *          |   |
  *       ___|___|___
@@ -206,10 +207,10 @@ app.post('/yellow21', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow22', function(req, res){
-  res.send("Color: Yellow, Row: 2, Col: 2");
+app.post('/yellow4', function(req, res){
+  res.send("Color: Yellow, Square: 4");
 })
- /** Board State: yellow23
+ /** Board State: yellow5
  * 
  *          |   |
  *       ___|___|___
@@ -219,10 +220,10 @@ app.post('/yellow22', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow23', function(req, res){
-  res.send("Color: Yellow, Row: 2, Col: 3");
+app.post('/yellow5', function(req, res){
+  res.send("Color: Yellow, Square: 5");
 })
- /** Board State: yellow31
+ /** Board State: yellow6
  * 
  *          |   |
  *       ___|___|___
@@ -232,10 +233,10 @@ app.post('/yellow23', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow31', function(req, res){
-  res.send("Color: Yellow, Row: 3, Col: 1");
+app.post('/yellow6', function(req, res){
+  res.send("Color: Yellow, Square: 6");
 })
- /** Board State: yellow32
+ /** Board State: yellow7
  * 
  *          |   |
  *       ___|___|___
@@ -245,10 +246,10 @@ app.post('/yellow31', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow32', function(req, res){
-  res.send("Color: Yellow, Row: 3, Col: 2");
+app.post('/yellow7', function(req, res){
+  res.send("Color: Yellow, Square: 7");
 })
- /** Board State: yellow33
+ /** Board State: yellow8
  * 
  *          |   |
  *       ___|___|___
@@ -258,8 +259,8 @@ app.post('/yellow32', function(req, res){
  *          |   |
  * 
  */
-app.post('/yellow33', function(req, res){
-  res.send("Color: Yellow, Row: 3, Col: 3");
+app.post('/yellow8', function(req, res){
+  res.send("Color: Yellow, Square: 8");
 })
 app.listen(port, '0.0.0.0', function () {
   console.log('Example app listening on port: ' + port + ' !');
