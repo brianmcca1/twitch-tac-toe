ce5d9fb02eb34001f53ac8b93d54da5570c31b73
mdduran <mdduran@outlook.com>
2017-10-11 14:22:29 -0400

    server that accepts cors

--- marco_code/tas-server.js
+++ marco_code/tas-server.js
@@ -3,7 +3,11 @@
 // 
 const express = require('express')
 const app = express()
-app.use(express.static('public'));
+app.use(express.static('public'), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const port = 8080;
 
